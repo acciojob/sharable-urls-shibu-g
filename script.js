5208d42040cb99c293d115c97dda13c28779cfcf
shibu-g <109884948+shibu-g@users.noreply.github.com>
2023-02-14 22:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 // your code here
+//var name=document.getElementById('name');
+//var year=document.getElementById('year');
+var btn=document.getElementById('submit');
+//var tex=document.getElementById('url');
+function fun(){
+	var name=document.getElementById('name');
+	var year=document.getElementById('year');
+	var tex=document.getElementById('url');
+	var url="https://localhost:8080";
+	var nam=name.value;
+	var yer=year.value;
+	alert("hi");
+	if(true){
+		tex.textContent=url+"?name="nam+"&year="+yer;
+	}
+	//alert(nam);
+	//tex.textContent=url+"?name="+nam;
+}
+btn.addEventListener('click',fun);
